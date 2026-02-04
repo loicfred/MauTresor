dd6530b5361eb0e7a4ad8a35f00bca9f5e16cc3a
Loic Fred.C <60476871+loicfred@users.noreply.github.com>
2026-02-04 22:58:07 +0400

    Update SVG path for culture icon in header

--- public/assets/fragments/header.php
+++ public/assets/fragments/header.php
@@ -234,7 +234,7 @@ use assets\obj\Notification;
                 } else if (data.type === "culture") {
                     resultBox.innerHTML += `<div class="d-flex p-2">
                                                               <svg viewBox="0 0 24 24" class="nav-icon">
-                                                                  <path fill="white" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
+                                                                  <path fill="white" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5l7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z"/>
                                                               </svg>
                                                               <a style="text-decoration: none; width: 100%;" href="/culture/${data.id}">${data.name}</a>
                                                             </div>`;
